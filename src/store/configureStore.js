9c8963a4f505653a00e45101789f91eb0dfae7ec
Eetu Sutinen <eetu.sutinen@gmail.com>
2015-12-14 09:10:03 +0200

    support hot loading store/reducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,5 +3,14 @@ import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState);
+
+  if(module.onReload) {
+    module.onReload(() => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer.default || nextReducer);
+      return true;
+    });
+  }
+
   return store;
 }
